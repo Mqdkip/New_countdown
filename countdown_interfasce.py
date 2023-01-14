818a7983a9e0c32ef9cbe427f63e3ae13571891d
mqdkip <rezpir0809@highgateschool.org.uk>
2023-01-14 00:09:54 +0000

    Numbers game implemented but needs improvement and working on.

--- countdown_interfasce.py
+++ countdown_interfasce.py
@@ -26,7 +26,9 @@ def making():
     numbers = [*los, *lob]
     global target
     target = randint(100, 999)
-    print(f"The aim is to make {target} using {numbers}")
+
+def making2():
+    print('aa')
     solutions = list()
     solve(target, numbers, list(), solutions)
     unique = list()
@@ -37,46 +39,8 @@ def making():
             unique.append(a)
             final.append(s)
     for s in final:  # print them out
-        #pass
         print(s)
     print(f"There are a total of {len(final)} solutions.")
-    # button_making(numbers)
-
-
-def switch():
-    if buttonc["state"] == "normal":
-        buttonc.configure(state="disabled")
-        button0.configure(state="disabled")
-        button1.configure(state="disabled")
-        button2.configure(state="disabled")
-        button3.configure(state="disabled")
-        button4.configure(state="disabled")
-        button5.configure(state="disabled")
-        plus.configure(state="normal")
-        minus.configure(state="normal")
-        multiply.configure(state="normal")
-        divide.configure(state="normal")
-
-    else:
-        buttonc.configure(state="normal")
-        button0.configure(state="normal")
-        button1.configure(state="normal")
-        button2.configure(state="normal")
-        button3.configure(state="normal")
-        button4.configure(state="normal")
-        button5.configure(state="normal")
-        plus.configure(state = "disabled")
-        minus.configure(state = "disabled")
-        multiply.configure(state = "disabled")
-        divide.configure(state = "disabled")
-
-
-def number_press_delete(b, n):
-    press(n)
-    switch()
-    b.grid_remove()
-
-
 def solve(target, numbers, path, solutions):
     if len(numbers) == 1:
         return
@@ -108,12 +72,40 @@ def combine(target, solutions, path, rem2, n1, n2, symb):
         solve(target, lst, newpath, solutions)
 
 
-# Button initialisation
+def switch():
+    if buttonc["state"] == "normal":
+        buttonc.configure(state="disabled")
+        button0.configure(state="disabled")
+        button1.configure(state="disabled")
+        button2.configure(state="disabled")
+        button3.configure(state="disabled")
+        button4.configure(state="disabled")
+        button5.configure(state="disabled")
+        plus.configure(state="normal")
+        minus.configure(state="normal")
+        multiply.configure(state="normal")
+        divide.configure(state="normal")
 
+    else:
+        buttonc.configure(state="normal")
+        button0.configure(state="normal")
+        button1.configure(state="normal")
+        button2.configure(state="normal")
+        button3.configure(state="normal")
+        button4.configure(state="normal")
+        button5.configure(state="normal")
+        plus.configure(state = "disabled")
+        minus.configure(state = "disabled")
+        multiply.configure(state = "disabled")
+        divide.configure(state = "disabled")
+
+def number_press_delete(b, n):
+    press(n)
+    switch()
+    b.grid_remove()
+# Button initialisation
 # On click of a button, the result enters the expression bar
 expression = ""
-
-
 def press(num):
     global expression
     expression = expression + str(num)  # Concatenation of string
@@ -143,23 +135,11 @@ def equalpress():
     else:
         print("Number evaluated is too far from target to score any points")
 
-
-
 # Function to clear the contents of text entry box
 def clear():
     global expression
     expression = ""
     equation.set("")
-
-
-# create a Buttons and place at a particular location inside the root window .
-# when user press the button, the command or function affiliated to that button is executed .
-
-# def show():
-#   label.config(text=clicked.get())
-#  dropdownsend.destroy()
-# print(clicked.get())
-
 
 def operand_press_switch(op):
     press(op)
@@ -169,147 +149,100 @@ def remember():
     if not button1.winfo_viewable():
         button1.grid()
 
-
-# def button_making(numbers):
 if __name__ == "__main__":
-    gui = ctk.CTk()  # create a GUI window
-    gui.configure(background="white")  # set the background colour of GUI window
-    gui.title("Countdown")  # set the title of GUI window
-    # gui.geometry("700x500")  # set the configuration of GUI window
+    gui2 = ctk.CTk()  # create a gui2 window
+    gui2.configure(background="white")  # set the background colour of gui2 window
+    gui2.title("Countdown")  # set the title of gui2 window
+    # gui2.geometry("700x500")  # set the configuration of gui2 window
     equation = StringVar()  # StringVar() is the variable class we create an instance of this class
-    expression_field = Entry(gui, textvariable=equation)  # create the text entry box for showing the expression.
+    expression_field = Entry(gui2, textvariable=equation)  # create the text entry box for showing the expression.
     expression_field.grid(columnspan=4,
                           ipadx=100)  # grid method is used for placing the widgets at respective positions in table like structure.
     making()
 
 brn = 0 #base row number
 
-
-
-
-target_label = ctk.CTkButton(gui, text = f"Target is {target}",text_color=("red"))
+target_label = ctk.CTkButton(gui2, text = f"Target is {target}",text_color=("red"))
 target_label.configure(state = "disabled", fg_color='red', text_color_disabled='white')
-target_label.grid(row = brn+1, column = 0)
+target_label.grid(row = brn+1, column = 0, padx = 10, pady = 10)
 
-buttonc = Button(gui, text=f'Constant button')
+buttonc = Button(gui2, text=f'Constant button')
 
-button0 = ctk.CTkButton(gui, text=f' {numbers[0]} ', font=('arial', 20, 'bold'))
+button0 = ctk.CTkButton(gui2, text=f' {numbers[0]} ', font=('arial', 20, 'bold'))
 button0.configure(command=functools.partial(number_press_delete, button0, numbers[0]))
-button0.grid(row=brn+3, column=0)
+button0.grid(row=brn+3, column=0, padx = 10, pady = 10)
 
-button1 = ctk.CTkButton(gui, text=f' {numbers[1]} ', font=('arial', 20, 'bold'))
+button1 = ctk.CTkButton(gui2, text=f' {numbers[1]} ', font=('arial', 20, 'bold'))
 button1.configure(command=functools.partial(number_press_delete, button1, numbers[1]))
-button1.grid(row=brn+3, column=1)
+button1.grid(row=brn+3, column=1, padx = 10, pady = 10)
 
-button2 = ctk.CTkButton(gui, text=f' {numbers[2]} ', font=('arial', 20, 'bold'))
+button2 = ctk.CTkButton(gui2, text=f' {numbers[2]} ', font=('arial', 20, 'bold'))
 button2.configure(command=functools.partial(number_press_delete, button2, numbers[2]))
-button2.grid(row=brn+3, column=2)
+button2.grid(row=brn+3, column=2, padx = 10, pady = 10)
 
-button3 = ctk.CTkButton(gui, text=f' {numbers[3]} ', font=('arial', 20, 'bold'))
+button3 = ctk.CTkButton(gui2, text=f' {numbers[3]} ', font=('arial', 20, 'bold'))
 button3.configure(command=functools.partial(number_press_delete, button3, numbers[3]))
-button3.grid(row=brn+4, column=0)
+button3.grid(row=brn+4, column=0, padx = 10, pady = 10)
 
-button4 = ctk.CTkButton(gui, text=f' {numbers[4]} ', font=('arial', 20, 'bold'))
+button4 = ctk.CTkButton(gui2, text=f' {numbers[4]} ', font=('arial', 20, 'bold'))
 button4.configure(command=functools.partial(number_press_delete, button4, numbers[4]))
-button4.grid(row=brn+4, column=1)
+button4.grid(row=brn+4, column=1, padx = 10, pady = 10)
 
-button5 = ctk.CTkButton(gui, text=f' {numbers[5]} ', font=('arial', 20, 'bold'))
+button5 = ctk.CTkButton(gui2, text=f' {numbers[5]} ', font=('arial', 20, 'bold'))
 button5.configure(command=functools.partial(number_press_delete, button5, numbers[5]))
-button5.grid(row=brn+4, column=2)
-
-# Create button, it will change label text
-# dropdownsend = Button(gui, text="Click to Confirm", command=show)
-# dropdownsend.grid(row=3, column=4)
-
-# Create Label
-#label = Label(gui, text=" ")
+button5.grid(row=brn+4, column=2, padx = 10, pady = 10)
 
 # operation buttons
-plus = ctk.CTkButton(gui, text=' + ')
+plus = ctk.CTkButton(gui2, text=' + ')
 plus.configure(command=functools.partial(operand_press_switch, ' + '))
 plus.grid(row=brn+3, column=3)
 
-minus = ctk.CTkButton(gui, text=' - ')
+minus = ctk.CTkButton(gui2, text=' - ')
 minus.configure(command=functools.partial(operand_press_switch, '-'))
 minus.grid(row=brn+4, column=3)
 
-multiply = ctk.CTkButton(gui, text=' * ')
+multiply = ctk.CTkButton(gui2, text=' * ')
 multiply.configure(command=functools.partial(operand_press_switch, '*'))
 multiply.grid(row=brn+5, column=3)
 
-divide = ctk.CTkButton(gui, text=' / ')
+divide = ctk.CTkButton(gui2, text=' / ')
 divide.configure(command=functools.partial(operand_press_switch, '/'))
 divide.grid(row=brn+6, column=3)
 
-equal = ctk.CTkButton(gui, text=' = ', command=equalpress)
-equal.grid(row=brn+6, column=2)
+equal = ctk.CTkButton(gui2, text=' = ', command=equalpress)
+equal.grid(row=brn+6, column=2, padx = 10, pady = 10)
 
-clear = ctk.CTkButton(gui, text='Clear', command=clear)
-clear.grid(row=brn+5, column='0')
+clear = ctk.CTkButton(gui2, text='Clear', command=clear)
+clear.grid(row=brn+5, column='0', padx = 10, pady = 10)
 
-
-undo_button = ctk.CTkButton(gui, text = 'Undo', font=('arial', 20, 'bold'))
+undo_button = ctk.CTkButton(gui2, text = 'Undo', font=('arial', 20, 'bold'))
 undo_button.configure(command=remember())
 undo_button.grid(row=brn+6, column=0)
 
-Lbracket = ctk.CTkButton(gui, text='(', command=lambda: press('('))
+Lbracket = ctk.CTkButton(gui2, text='(', command=lambda: press('('))
 Lbracket.grid(row=brn+5, column=1)
 
-Rbracket = ctk.CTkButton(gui, text=')', command=lambda: press(')'))
+Rbracket = ctk.CTkButton(gui2, text=')', command=lambda: press(')'))
 Rbracket.grid(row=brn+5, column=2)
 
-#reset = ctk.CTkButton(gui, text='Reset', command=making_numbers)
-#reset.grid(row=brn+6, column=1)
-
-timer_string_var = ctk.StringVar(gui)
-time_b = ctk.CTkButton(gui, textvariable=timer_string_var)
+timer_string_var = ctk.StringVar(gui2)
+time_b = ctk.CTkButton(gui2, textvariable=timer_string_var)
 time_b.configure(state="disabled", fg_color='red', text_color_disabled='white')
 time_b.grid(row=brn + 1, column=1)
 
-t_seconds = 30
-
-
-def update_gui():
+t_seconds = 3
+def update_gui2():
     global t_seconds
     timer = datetime.timedelta(seconds=t_seconds)
     timer_string_var.set(timer)
     t_seconds -= 1
     if (t_seconds == 0):
         messagebox.showinfo("Time Countdown", "Time's up ")
-    gui.update()
-    gui.after(1000, update_gui)
+    gui2.update()
+    gui2.after(1000, update_gui2)
 
-gui.after(1000, update_gui)
-
-
+gui2.after(1000, update_gui2)
 
 
-
-
-# while t_seconds > 0:
-#     timer = datetime.timedelta(seconds = t_seconds)
-#     time.sleep(1)
-#     t_seconds -= 1
-#     time_b = ctk.CTkButton(gui, text=f"Time left is {timer}")
-#     time_b.configure(state="disabled", fg_color='red', text_color_disabled='white')
-#     time_b.grid(row=brn + 1, column=1)
-#     gui.update()
-
-
-
-
-
-gui.mainloop()  # start the GUI
-
-# nobtions = [
-#     "1",
-#     "2",
-#     "3",
-#     "4", ]
-#
-# clicked = IntVar()
-# clicked.set("Number of Big Numbers")
-#
-# # Create Dropdown menu
-# dropdown = OptionMenu(gui, clicked, *nobtions)
-# dropdown.grid(row=2, column=4)
+making2()
+gui2.mainloop()  # start the gui2
